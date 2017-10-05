59f47909ec7d29d48b8fe9b0c4e17dcbc821acaf
Adrian Sirko <cipko@Adrians-MacBook-Air.local>
2017-10-05 17:50:00 -0400

    LIRI

--- liri.js
+++ liri.js
@@ -2,26 +2,38 @@ var Twitter = require('twitter');
 
 var tweetkeys = require("./keys"); 
 
+var Spotify = require('node-spotify-api');
+
 var tweetuser = new Twitter(tweetkeys);
 
+var request = require("request");
 
+var fs = require("fs");
+
+ 
 var liricommand = process.argv[2];
+var spotsearch = process.argv[3];
+
+var spotifySong = new Spotify({
+  id: '21050ce5bc844471b195ce08b8ecbe73',
+  secret: 'ec9d203597e7471184888900ff84015a'
+});
 
 switch (liricommand) {
   case "my-tweets":
     tweets();
     break;
 
-  case "deposit":
-    deposit();
+  case "spotify-this-song":
+    spotify();
     break;
 
-  case "withdraw":
-    withdraw();
+  case "movie-this":
+    movie();
     break;
 
-  case "lotto":
-    lotto();
+  case "do-what-it-says":
+    command();
     break;
 }
 function tweets() {
@@ -36,3 +48,46 @@ tweetuser.get('statuses/user_timeline', params, function(error, tweets, response
 }
 });
     }
+function spotify(){
+	spotifySong.search({ type: 'track', query: spotsearch }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(JSON.stringify(data,null,2)); 
+});
+}
+function movie (){
+	var queryUrl = "http://www.omdbapi.com/?t=" + spotsearch + "&y=&plot=short&apikey=40e9cece";
+	request(queryUrl, function(error, response, body) {
+		console.log(JSON.stringify(body, null, 2))
+  	if (!error && response.statusCode === 200) {
+    console.log("Release Year: " + JSON.parse(body).Year);
+	  }
+	});
+}
+function command (){
+fs.readFile("random.txt", "utf8", function(error, data) {
+
+  if (error) {
+    return console.log(error);
+  }
+
+
+  console.log(data);
+
+
+  var dataArr = data.split(",");
+
+  console.log(dataArr);
+
+  spotifySong.search({ type: 'track', query: dataArr[1] }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(JSON.stringify(data,null,2)); 
+});
+
+});
+}
